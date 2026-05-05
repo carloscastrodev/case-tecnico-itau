7f070893727fb7185f9cd36074e179e43a1ee17c
Carlos <carlos_2009_vinicius@hotmail.com>
2026-05-05 23:38:20 +0000

    deps[dynamosee]: wire dynamoose module

--- src/libs.module.ts
+++ src/libs.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
 import { isProduction } from '@/utils/environment';
+import { DynamooseModule } from 'nestjs-dynamoose';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './config/validate';
 
 @Module({
   imports: [
@@ -10,8 +13,19 @@ import { isProduction } from '@/utils/environment';
         transport: isProduction() ? undefined : { target: 'pino-pretty' },
       },
     }),
+    DynamooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService<Env>) => ({
+        aws: {
+          accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+          region: configService.get('AWS_REGION'),
+        },
+        local: configService.get('DYNAMODB_ENDPOINT'),
+      }),
+    }),
   ],
   controllers: [],
   providers: [],
 })
-export class LibsModule { }
+export class LibsModule {}
