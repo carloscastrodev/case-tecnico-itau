493df33ddd374bc3ca76bacfadb94cc92913bc6e
Carlos <carlos_2009_vinicius@hotmail.com>
2026-05-07 18:23:50 +0000

    tests: include more tests for authenticated guard, update constructor signature

--- src/guards/tests/authenticated.guard.spec.ts
+++ src/guards/tests/authenticated.guard.spec.ts
@@ -2,6 +2,9 @@ import { executionContextFactory } from '@/tests/fixtures/execution-context';
 import { UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AuthenticatedGuard } from '../authenticated.guard';
+import { ConfigService } from '@nestjs/config';
+import { Env } from '@/config/validate';
+import { Request } from 'express';
 
 describe('Authenticated Guard [UNIT]', () => {
   const testSecret = 'secret';
@@ -11,6 +14,7 @@ describe('Authenticated Guard [UNIT]', () => {
 
   let guard: AuthenticatedGuard;
   let jwtService: JwtService;
+  let configService: ConfigService<Env>;
 
   beforeAll(() => {
     jwtService = new JwtService({
@@ -22,7 +26,14 @@ describe('Authenticated Guard [UNIT]', () => {
       },
     });
 
-    guard = new AuthenticatedGuard(jwtService);
+    configService = new ConfigService<Env>();
+
+    guard = new AuthenticatedGuard(jwtService, configService);
+  });
+
+  beforeEach(() => {
+    configService.set('JWT_AUDIENCE', testAudience);
+    configService.set('JWT_ISSUER', testIssuer);
   });
 
   it('should throw unauthorized exception when no authorization header is not set', async () => {
@@ -57,9 +68,34 @@ describe('Authenticated Guard [UNIT]', () => {
     await expect(guard.canActivate(mockRequestContext)).rejects.toThrow(UnauthorizedException);
   });
 
+  it('should throw unauthorized exception when audience is invalid', async () => {
+    configService.set('JWT_AUDIENCE', 'another-audience');
+
+    const token = jwtService.sign({ username: 'user' });
+    const mockRequestContext = executionContextFactory({ headers: { authorization: `Bearer ${token}` } });
+    await expect(guard.canActivate(mockRequestContext)).rejects.toThrow(UnauthorizedException);
+  });
+
+  it('should throw unauthorized exception when issuer is invalid', async () => {
+    configService.set('JWT_ISSUER', 'another-issuer');
+
+    const token = jwtService.sign({ username: 'user' });
+    const mockRequestContext = executionContextFactory({ headers: { authorization: `Bearer ${token}` } });
+    await expect(guard.canActivate(mockRequestContext)).rejects.toThrow(UnauthorizedException);
+  });
+
   it('should return true when authorization header is set and token is valid', async () => {
     const token = jwtService.sign({ username: 'user' });
     const mockRequestContext = executionContextFactory({ headers: { authorization: `Bearer ${token}` } });
     await expect(guard.canActivate(mockRequestContext)).resolves.toBe(true);
+  });
+
+  it('should set the user on the request when token is valid', async () => {
+    const token = jwtService.sign({ username: 'user' });
+    const mockRequestContext = executionContextFactory({ headers: { authorization: `Bearer ${token}` } });
+    await guard.canActivate(mockRequestContext);
+    const request = mockRequestContext.switchToHttp().getRequest<Request>();
+    expect(request.user).toBeDefined();
+    expect(request.user?.username).toBe('user');
   });
 });
