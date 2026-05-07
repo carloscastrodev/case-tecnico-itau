0e0fbeb501d8bd3572e5c532621487d29241049c
Carlos <carlos_2009_vinicius@hotmail.com>
2026-05-07 02:01:18 +0000

    chore: add guards module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MessagesModule } from '@/modules/messages/messages.module';
 import { LibsModule } from '@/libs.module';
 import { ConfigModule } from '@nestjs/config';
 import { validateEnv } from '@/config/validate';
+import { GuardsModule } from './guards/guards.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { validateEnv } from '@/config/validate';
       isGlobal: true,
       validate: validateEnv,
     }),
+    GuardsModule,
     LibsModule,
     AuthModule,
     MessagesModule,
